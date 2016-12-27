7d4c3897ea3851f08a04355c46e20824cc6ac003
Arnas Dundulis <arnas@vinted.com>
2016-12-27 16:44:43 +0200

    Add empty framework project

---
+++ vinted-ab-ios/vinted-ab-ios/vinted-ab-ios.h
@@ -0,0 +1,19 @@
+//
+//  vinted-ab-ios.h
+//  vinted-ab-ios
+//
+//  Created by Arnas Dundulis on 12/27/16.
+//  Copyright © 2016 Vinted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for vinted-ab-ios.
+FOUNDATION_EXPORT double vinted_ab_iosVersionNumber;
+
+//! Project version string for vinted-ab-ios.
+FOUNDATION_EXPORT const unsigned char vinted_ab_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <vinted_ab_ios/PublicHeader.h>
+
+
