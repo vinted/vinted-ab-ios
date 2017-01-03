bc0d91d15866ac650ee07bed6df2f1d49fa6c1fc
Arnas Dundulis <arnas@vinted.com>
2017-01-03 08:38:45 +0200

    Remove Unbox related code

--- vinted-ab-ios/vinted-ab-ios/Extensions/VNTModel+Testable.swift
+++ vinted-ab-ios/vinted-ab-ios/Extensions/VNTModel+Testable.swift
@@ -1,28 +1,6 @@
 import Foundation
-import Unbox
 
 private let NumberedJsonFileFormat = "%@[%@]"
-
-public extension Unboxable {
-    static func mappedObject(bundle: Bundle, index: String, dictChanges: [(key: String, value: AnyObject?)] = []) -> Self? {
-        guard let dict = jsonFileDictionary(forType: self,
-                                            bundle: bundle,
-                                            index: index) else { return nil }
-        
-        var updatedDict = dict
-        for dictChange in dictChanges {
-            updatedDict = updateDictionary(dict: dict as [String : AnyObject], keyPath: dictChange.key, value: dictChange.value)
-        }
-        return try? unbox(dictionary: updatedDict)
-    }
-    
-    static func loadSample(_ bundle: Bundle, index: String = "0", replace key: String? = nil, with value: AnyObject? = nil) -> Self? {
-        if let key = key {
-            return mappedObject(bundle: bundle, index: index, dictChanges: [(key: key, value: value)])
-        }
-        return mappedObject(bundle: bundle, index: index)
-    }
-}
 
 public extension VNTModel {
     static func mappedObject(bundle: Bundle, index: String, dictChanges: [(key: String, value: AnyObject?)] = []) -> Self? {
@@ -63,43 +41,17 @@ private func updateDictionary(dict: [String: AnyObject], keyPath: String, value:
     return dictToEdit
 }
 
-public extension UnboxableWithContext {
-    static func mappedObject(bundle: Bundle, index: String, mappingContext: UnboxContext) -> Self? {
-        guard let dict = jsonFileDictionary(forType: self,
-                                            bundle: bundle,
-                                            index: index) else { return nil }
-        do {
-            let model: Self = try unbox(dictionary: dict, context: mappingContext)
-            return model
-        } catch {
-            return nil
-        }
-    }
-}
-
-public func modelMapping<T: Unboxable>(fromBundle bundle: Bundle, index: String) -> (instance: T, dictionary: UnboxableDictionary)? {
-    guard let dict = jsonFileDictionary(forType: T.self, bundle: bundle, index: index) else {
-        return nil
-    }
-    do {
-        let value = try unbox(dictionary: dict) as T
-        return (instance: value, dictionary: dict)
-    } catch {
-        return nil
-    }
-}
-
 public func jsonFileString(forType type: Any, bundle: Bundle, index: String) -> String? {
     guard let data = jsonFileData(forType: type, bundle: bundle, index: index) else { return nil }
     return String(data: data, encoding: String.Encoding.utf8)
 }
 
-public func jsonFileDictionary(forType type: Any, bundle: Bundle, index: String) -> UnboxableDictionary? {
+public func jsonFileDictionary(forType type: Any, bundle: Bundle, index: String) -> [String : Any]? {
     guard let data = jsonFileData(forType: type, bundle: bundle, index: index) else { return nil }
-    var dict: UnboxableDictionary?
+    var dict: [String : Any]?
     do {
         dict = try JSONSerialization.jsonObject(with: data,
-                                                options: JSONSerialization.ReadingOptions.mutableContainers) as? UnboxableDictionary
+            options: JSONSerialization.ReadingOptions.mutableContainers) as? [String : Any]
     } catch {
         return nil
     }
